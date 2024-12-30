565bc9b3c724df10dbda9103f0834665b4a24d5e
ReneVajda1 <rene.vajda@yahoo.com>
2024-12-31 00:59:39 +0100

    diagonal movement created

--- game/src/entity/Entity.java
+++ game/src/entity/Entity.java
@@ -6,7 +6,8 @@ public class Entity {
     public int x, y;
     public int speed;
 
-    public BufferedImage up1, down1, left1, right1, up2, down2, left2, right2;
+    public BufferedImage up1, down1, left1, right1, up2, down2, left2, right2,
+            upLeft1, upRight1, downLeft1, downRight1, upLeft2, upRight2, downLeft2, downRight2;
     public String direction;
     public int spriteCounter = 0;
     public int spriteNum = 1;
--- game/src/entity/Player.java
+++ game/src/entity/Player.java
@@ -28,84 +28,125 @@ public class Player extends Entity {
     // this makes SPRITE IMAGES POSSIBLE
     public void getPlayerImage(){
         try {
-            up1 = ImageIO.read(getClass().getResourceAsStream("/img/stolenBack.png"));
-            up2 = ImageIO.read(getClass().getResourceAsStream("/img/stolenBackWalk.png"));
-            down1 = ImageIO.read(getClass().getResourceAsStream("/img/stolenFront.png"));
-            down2 = ImageIO.read(getClass().getResourceAsStream("/img/stolenFrontWalk.png"));
-            right1 = ImageIO.read(getClass().getResourceAsStream("/img/rightStolen.png"));
-            right2 = ImageIO.read(getClass().getResourceAsStream("/img/rightStolenWalk.png"));
-            left1 = ImageIO.read(getClass().getResourceAsStream("/img/leftStolen.png"));
-            left2 = ImageIO.read(getClass().getResourceAsStream("/img/leftStolenWalk.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-walk.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-idle.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-walk.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-idle.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-walk.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-walk.png"));
+
+            upLeft1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            upRight1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-walk.png"));
+            downLeft1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            downRight1 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-walk.png"));
+
+            upLeft2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            upRight2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-idle.png"));
+
+            downLeft2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-left-idle.png"));
+            downRight2 = ImageIO.read(getClass().getResourceAsStream("/img/barb-right-idle.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public void update() {
-        // Update direction and movement based on key inputs
-        if (keyH.wPressed) {
-            direction = "up"; // Set direction to up
-            y -= speed; // Move up
+        // Adjust diagonal movement speed
+        double diagonalSpeed = speed / Math.sqrt(2);
+
+        // Movement logic
+        boolean isMoving = false;
+        if (keyH.wPressed && keyH.aPressed) {
+            direction = "upLeft";
+            x -= diagonalSpeed;
+            y -= diagonalSpeed;
+            isMoving = true;
+        } else if (keyH.wPressed && keyH.dPressed) {
+            direction = "upRight";
+            x += diagonalSpeed;
+            y -= diagonalSpeed;
+            isMoving = true;
+        } else if (keyH.sPressed && keyH.aPressed) {
+            direction = "downLeft";
+            x -= diagonalSpeed;
+            y += diagonalSpeed;
+            isMoving = true;
+        } else if (keyH.sPressed && keyH.dPressed) {
+            direction = "downRight";
+            x += diagonalSpeed;
+            y += diagonalSpeed;
+            isMoving = true;
+        } else if (keyH.wPressed) {
+            direction = "up";
+            y -= speed;
+            isMoving = true;
         } else if (keyH.sPressed) {
-            direction = "down"; // Set direction to down
-            y += speed; // Move down
+            direction = "down";
+            y += speed;
+            isMoving = true;
         } else if (keyH.aPressed) {
-            direction = "left"; // Set direction to left
-            x -= speed; // Move left
+            direction = "left";
+            x -= speed;
+            isMoving = true;
         } else if (keyH.dPressed) {
-            direction = "right"; // Set direction to right
-            x += speed; // Move right
+            direction = "right";
+            x += speed;
+            isMoving = true;
         }
+
+        // Handle sprite animation
         spriteCounter++;
-        if (spriteCounter>15) {
-            if (spriteNum==1) {
+        if (spriteCounter > 15) { // Change animation every 15 updates
+            if (spriteNum == 1) {
                 spriteNum = 2;
-            } else if (spriteNum==2) {
-                spriteNum=1;
+            } else {
+                spriteNum = 1;
             }
-            spriteCounter=0;
+            spriteCounter = 0;
+        }
+
+        // If not moving, set to idle pose
+        if (!isMoving) {
+            spriteNum = 1; // Default idle pose
         }
     }
-
     public void draw(Graphics2D g2) {
-        //     g2.setColor(Color.white); // Set color for player
-        //     g2.fillRect(x, y, gp.tileSize, gp.tileSize); // Draw player as rectangle
         BufferedImage image = null;
 
+        // Switch images based on direction and sprite number
         switch (direction) {
             case "up":
-                if (spriteNum==1){
-                    image=up1;
-                }
-                if (spriteNum==2){
-                    image=up2;
-                }
+                if (spriteNum == 1) image = up1;
+                else if (spriteNum == 2) image = up2;
                 break;
-
             case "down":
-                if (spriteNum==1){
-                    image=down1;
-                }
-                if (spriteNum==2){
-                    image=down2;
-                }
+                if (spriteNum == 1) image = down1;
+                else if (spriteNum == 2) image = down2;
                 break;
-
             case "left":
-                if (spriteNum==1){
-                    image=left1;
-                }
-                if (spriteNum==2){
-                    image=left2;
-                }
+                if (spriteNum == 1) image = left1;
+                else if (spriteNum == 2) image = left2;
                 break;
-
             case "right":
-                if (spriteNum==1){
-                    image=right1;
-                }
-                if (spriteNum==2){
-                    image=right2;
-                }
+                if (spriteNum == 1) image = right1;
+                else if (spriteNum == 2) image = right2;
+                break;
+            case "upLeft":
+                if (spriteNum == 1) image = upLeft1;
+                else if (spriteNum == 2) image = upLeft2;
+                break;
+            case "upRight":
+                if (spriteNum == 1) image = upRight1;
+                else if (spriteNum == 2) image = upRight2;
+                break;
+            case "downLeft":
+                if (spriteNum == 1) image = downLeft1;
+                else if (spriteNum == 2) image = downLeft2;
+                break;
+            case "downRight":
+                if (spriteNum == 1) image = downRight1;
+                else if (spriteNum == 2) image = downRight2;
                 break;
         }
         // CHARACTER SIZE
