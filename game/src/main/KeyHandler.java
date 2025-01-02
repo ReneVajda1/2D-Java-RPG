44a2ceb41339fe4bb134eef9571bf0dffabda5fb
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-02 03:57:29 +0100

    keyhandler updated

--- game/src/main/KeyHandler.java
+++ game/src/main/KeyHandler.java
@@ -4,26 +4,24 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyHandler implements KeyListener {
-    public boolean wPressed, aPressed, sPressed, dPressed; // Key states
+    public boolean wPressed, aPressed, sPressed, dPressed;
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int code = e.getKeyCode(); // Get key code
-
-        if (code == KeyEvent.VK_W) wPressed = true; // W key pressed
-        if (code == KeyEvent.VK_S) sPressed = true; // S key pressed
-        if (code == KeyEvent.VK_A) aPressed = true; // A key pressed
-        if (code == KeyEvent.VK_D) dPressed = true; // D key pressed
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) wPressed = true;
+        if (code == KeyEvent.VK_S) sPressed = true;
+        if (code == KeyEvent.VK_A) aPressed = true;
+        if (code == KeyEvent.VK_D) dPressed = true;
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        int code = e.getKeyCode(); // Get key code
-
-        if (code == KeyEvent.VK_W) wPressed = false; // W key released
-        if (code == KeyEvent.VK_S) sPressed = false; // S key released
-        if (code == KeyEvent.VK_A) aPressed = false; // A key released
-        if (code == KeyEvent.VK_D) dPressed = false; // D key released
+        int code = e.getKeyCode();
+        if (code == KeyEvent.VK_W) wPressed = false;
+        if (code == KeyEvent.VK_S) sPressed = false;
+        if (code == KeyEvent.VK_A) aPressed = false;
+        if (code == KeyEvent.VK_D) dPressed = false;
     }
 
     @Override
