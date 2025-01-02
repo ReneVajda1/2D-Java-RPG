650fd8c7e26831ad56a68548a1467e509363b2dc
ReneVajda1 <rene.vajda@yahoo.com>
2025-01-02 03:56:22 +0100

    entity updated -> buffered images to spritesheets

--- game/src/entity/Entity.java
+++ game/src/entity/Entity.java
@@ -3,15 +3,14 @@ package entity;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-    public int x, y;
-    public int speed;
+    public int x, y; // Position
+    public int speed; // Speed of movement
 
-    public BufferedImage up1, down1, left1, right1, up2, down2, left2, right2,
-            upLeft1, upRight1, downLeft1, downRight1, upLeft2, upRight2, downLeft2, downRight2,
+    // Sprite sheets for animations
+    public BufferedImage walkingSheet;
+    public BufferedImage idleSheet;
 
-            right3;
-
-    public String direction;
-    public int spriteCounter = 0;
-    public int spriteNum = 1;
+    public String direction; // Current direction
+    public int spriteCounter = 0; // Timer for frame switching
+    public int spriteNum = 1; // Current frame number
 }
